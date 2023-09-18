4a13b98f93b98445f4895f3dd858e6283f27de73
Sbelgana <belganasaad@gmail.com>
2023-09-17 23:26:17 -0400

    init

--- Ex_01.py
+++
@@ -1,7 +0,0 @@
-# Demande à l'utilisateur d'entrer son prenom.
-
-# Demande à l'utilisateur d'entrer son nom.
-
-# Affiche un message de salutation en utilisant les valeurs saisies pour le prenom et le nom.
-
-
